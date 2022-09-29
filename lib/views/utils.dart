25ae9e3704512c72e85db667fae9a19f06549969
Doleshor <doleshorkhad@gmail.com>
2022-09-29 13:49:58 +0545

    utils

--- lib/views/utils.dart
+++ lib/views/utils.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Utils {
-  static List<Widget> modelBuilder<M>(
-          List<M> models, Widget Function(int index, M model) builder) =>
-      models
-          .asMap()
-          .map<int, Widget>(
-              (index, model) => MapEntry(index, builder(index, model)))
-          .values
-          .toList();
-}
+// class Utils {
+//   static List<Widget> modelBuilder<M>(
+//           List<M> models, Widget Function(int index, M model) builder) =>
+//       models
+//           .asMap()
+//           .map<int, Widget>(
+//               (index, model) => MapEntry(index, builder(index, model)))
+//           .values
+//           .toList();
+// }
 
 class Methods {
   static List<Widget> modelBuilder<M>(
